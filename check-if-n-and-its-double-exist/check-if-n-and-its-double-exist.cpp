0a0e23a973811917be7a772c424e6d5570d0fd04
Raemin Kang <foals18@gmail.com>
2023-06-20 10:26:11 +0900

    Time: 11 ms, Memory: 10 MB - LeetHub

--- check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     bool checkIfExist(vector<int> &arr) {
-        map<int, vector<int>> m;
-        for (int i = 0; i < arr.size(); i++) {
-            m[arr[i]].push_back(i);
-        }
-        for (int i = 0; i < arr.size(); i++) {
-            if (m.count(arr[i] * 2)) {
-                for (const int idx : m[arr[i] * 2]) {
-                    if (idx != i) {
-                        return true;
-                    }
-                }
+        for (size_t i = 0; i < arr.size(); i++) {
+            auto it = find(arr.cbegin(), arr.cend(), arr[i] * 2);
+            auto dist = distance(arr.cbegin(), it);
+            if (it != arr.cend() && dist != i) {
+                return true;
             }
         }
         return false;
